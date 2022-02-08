35c2c7bef70f22f4096ef92c3f73615606477d7c
AdamRohdell <rohdell@student.chalmers.se>
2022-02-08 16:15:21 +0100

    started on sound engine

--- 1.0.0/Shard/ConsoleApp1/Shard/Sound.cs
+++ 1.0.0/Shard/ConsoleApp1/Shard/Sound.cs
@@ -6,11 +6,32 @@
 *   
 */
 
+using System.Media;
+
 namespace Shard
 {
     public class Sound        
     {
+        private SoundPlayer player;
 
+        public Sound()
+        {
+            player = new SoundPlayer();
+        }
+
+        //Plays a .wav file asynchronosly from the specified filepath
+        public void PlaySound(string path)
+        {
+            player.SoundLocation = path;
+            player.Play();
+        }
+
+        //Plays a .wav file synchronosly from the specified filepath
+        public void PlaySoundSync(string path)
+        {
+            player.SoundLocation = path;
+            player.PlaySync();
+        }
 
     }
 }
